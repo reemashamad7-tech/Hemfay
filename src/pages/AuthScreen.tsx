38ffa7125d1e8094627dab9f2c4a5ea6dc2f9cf8
reemashamad7-tech <reemashamad7@gmail.com>
2026-08-19 08:05:29 +0300

    Update AuthScreen.tsx

--- src/pages/AuthScreen.tsx
+++ src/pages/AuthScreen.tsx
@@ -4,7 +4,7 @@ import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import * as z from 'zod';
 import { useStore } from '../store/useStore';
-import { HemfayLogo } from '../components/HemafyLogo';
+import { HemafyLogo } from '../components/HemafyLogo';
 import { Mail, Lock, User, AlertCircle } from 'lucide-react';
 import { motion, AnimatePresence } from 'framer-motion';
 
